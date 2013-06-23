46bad60e61262071244b0f81518ac86141887c4d
Oiawesome <Oiawesome99@yahoo.com>
2013-06-23 17:37:24 -0600

    Create formats-data.js

---
+++ mods/nuv2/formats-data.js
@@ -0,0 +1,54 @@
+exports.BattleFormatsData = {
+  luxray: {
+    inherit: true,
+  	tier: "RU"
+	},
+	jynx: {
+		inherit: true,
+		tier: "NU"
+	},
+	gothitelle: {
+		inherit: true,
+		tier: "OU"
+	},
+	archeops: {
+		inherit: true,
+		tier: "OU"
+	},
+	weavile: {
+		inherit: true,
+		tier: "OU"
+	},
+	politoed: {
+		inherit: true,
+		tier: "UU"
+	},
+	ninetales: {
+		inherit: true,
+		tier: "UU"
+	},
+	darmanitan: {
+		inherit: true,
+		tier: "RU"
+	},
+	gligar: {
+		inherit: true,
+		tier: "RU"
+	},
+	roselia: {
+		inherit: true,
+		tier: "RU"
+	},
+	staraptor: {
+		inherit: true,
+		tier: "UU"
+	},
+	gliscor: {
+		inherit: true,
+		tier: "UU"
+	},
+	gastrodon: {
+		inherit: true,
+		tier: "UU"
+	}
+};
