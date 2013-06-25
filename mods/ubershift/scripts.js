bd77c49ec49a602b9d7e1d2b068e151b586f8253
Oiawesome <Oiawesome99@yahoo.com>
2013-06-24 23:03:28 -0600

    Update scripts.js

--- mods/ubershift/scripts.js
+++ mods/ubershift/scripts.js
@@ -5,19 +5,23 @@ exports.BattleScripts = {
 			var adjustment = 0;
 			if (this.data.FormatsData[i]) tier = this.data.FormatsData[i].tier;
 			switch (tier) {
+			case 'Uber':        
+			        adjustment = 25;
+				break;
+			case 'OU':
 			case 'BL':
 			case 'UU':
-				adjustment = 5;
+				adjustment = 30;
 				break;
 			case 'BL2':
 			case 'RU':
-				adjustment = 10;
+				adjustment = 35;
 				break;
 			case 'BL3':
 			case 'NU':
 			case 'NFE':
 			case 'LC':
-				adjustment = 15;
+				adjustment = 40;
 			}
 
 			if (adjustment) {
