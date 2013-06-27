fba6f172fbb72c609ae345edcff78a2e111cb79b
mrSmellyfeet100 <boraoxal@gmail.com>
2013-06-27 18:37:44 +0200

    Update scripts.js

--- mods/ubershift/scripts.js
+++ mods/ubershift/scripts.js
@@ -3,29 +3,31 @@ exports.BattleScripts = {
                 for (var i in this.data.Pokedex) {
                         var tier = '';
                         var adjustment = 0;
-                        if (this.data.FormatsData[i]) tier = this.data.FormatsData[i].tier;
-                        switch (tier) {
-                        case 'OU':
-                                adjustment = 7;
-                                break;
-                        case 'BL':
-                        case 'UU':
-                                adjustment = 15;
-                                break;
-                        case 'BL2':
-                        case 'RU':
-                                adjustment = 20;
-                                break;
-                        case 'BL3':
-                        case 'NU':
-                        case 'NFE':
-                                adjustment = 26;
-                                break;
-                        case 'LC':
-                                adjustment = 35;
-                        }
-
-                        if (adjustment) {
+       if (this.data.FormatsData[i]) tier = this.data.FormatsData[i].tier;
+       switch (tier) {
++      case 'Uber':        
++              adjustment = 25;
++        break;
++      case 'OU':
+       case 'BL':
+       case 'UU':
+-        adjustment = 5;
++        adjustment = 30;
+         break;
+       case 'BL2':
+       case 'RU':
+-        adjustment = 10;
++        adjustment = 35;
+         break;
+       case 'BL3':
+       case 'NU':
+       case 'NFE':
+       case 'LC':
+-        adjustment = 15;
++        adjustment = 40;
+       }
+ 
+       if (adjustment) {
                                 this.data.Pokedex[i].baseStats.hp += adjustment;
                                 this.data.Pokedex[i].baseStats.atk += adjustment;
                                 this.data.Pokedex[i].baseStats.def += adjustment;
