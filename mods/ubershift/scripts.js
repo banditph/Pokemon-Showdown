b8b1b3bbdac7fb7369ce7f9b4dc1f4ab47fe3c04
mrSmellyfeet100 <boraoxal@gmail.com>
2013-06-27 18:34:43 +0200

    Update scripts.js
    subwayshift boosts

--- mods/ubershift/scripts.js
+++ mods/ubershift/scripts.js
@@ -1,37 +1,38 @@
 exports.BattleScripts = {
-  init: function() {
-		for (var i in this.data.Pokedex) {
-			var tier = '';
-			var adjustment = 0;
-			if (this.data.FormatsData[i]) tier = this.data.FormatsData[i].tier;
-			switch (tier) {
-			case 'Uber':        
-			        adjustment = 25;
-				break;
-			case 'OU':
-			case 'BL':
-			case 'UU':
-				adjustment = 30;
-				break;
-			case 'BL2':
-			case 'RU':
-				adjustment = 35;
-				break;
-			case 'BL3':
-			case 'NU':
-			case 'NFE':
-			case 'LC':
-				adjustment = 40;
-			}
+        init: function() {
+                for (var i in this.data.Pokedex) {
+                        var tier = '';
+                        var adjustment = 0;
+                        if (this.data.FormatsData[i]) tier = this.data.FormatsData[i].tier;
+                        switch (tier) {
+                        case 'OU':
+                                adjustment = 7;
+                                break;
+                        case 'BL':
+                        case 'UU':
+                                adjustment = 15;
+                                break;
+                        case 'BL2':
+                        case 'RU':
+                                adjustment = 20;
+                                break;
+                        case 'BL3':
+                        case 'NU':
+                        case 'NFE':
+                                adjustment = 26;
+                                break;
+                        case 'LC':
+                                adjustment = 35;
+                        }
 
-			if (adjustment) {
-				this.data.Pokedex[i].baseStats.hp += adjustment;
-				this.data.Pokedex[i].baseStats.atk += adjustment;
-				this.data.Pokedex[i].baseStats.def += adjustment;
-				this.data.Pokedex[i].baseStats.spa += adjustment;
-				this.data.Pokedex[i].baseStats.spd += adjustment;
-				this.data.Pokedex[i].baseStats.spe += adjustment;
-			}
-		}
-	}
+                        if (adjustment) {
+                                this.data.Pokedex[i].baseStats.hp += adjustment;
+                                this.data.Pokedex[i].baseStats.atk += adjustment;
+                                this.data.Pokedex[i].baseStats.def += adjustment;
+                                this.data.Pokedex[i].baseStats.spa += adjustment;
+                                this.data.Pokedex[i].baseStats.spd += adjustment;
+                                this.data.Pokedex[i].baseStats.spe += adjustment;
+                        }
+                }
+        }
 };
